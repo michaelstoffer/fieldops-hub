ab4cc12d417d88475879e291df7d3b9bf944dc9a
Michael Stoffer <mstoffer@michaelstoffer.com>
2026-05-18 18:10:23 -0400

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ use App\Services\SmsService;
 use App\Services\TemplateRenderer;
 use App\Services\TwilioSmsService;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -35,6 +36,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }    
+        
         Vite::prefetch(concurrency: 3);
 
         Event::listen(JobCreated::class, SendJobConfirmationEmail::class);

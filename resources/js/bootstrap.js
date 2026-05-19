e8881ed45487aa36f84fb776155615bb9ab76951
Michael Stoffer <mstoffer@michaelstoffer.com>
2026-05-18 21:12:55 -0400

    Configure Laravel Echo for Reverb broadcasting.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,17 @@
-// resources/js/bootstrap.js
-
 import axios from 'axios';
+import Echo from 'laravel-echo';
 
 if (typeof window !== 'undefined') {
     window.axios = axios;
     window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-}
 
-// If you want lodash too:
-// import _ from 'lodash';
-// window._ = _;
+    window.Echo = new Echo({
+        broadcaster: 'reverb',
+        key: import.meta.env.VITE_REVERB_APP_KEY,
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+        enabledTransports: ['ws', 'wss'],
+    });
+}
